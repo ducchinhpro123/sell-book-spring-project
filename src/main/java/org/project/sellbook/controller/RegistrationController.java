766df20b0a5a1a0356647ec9508d2d24101bd79e
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-09-10 14:50:45 +0700

    fix: duplicate username are not accepted

--- src/main/java/org/project/sellbook/controller/RegistrationController.java
+++ src/main/java/org/project/sellbook/controller/RegistrationController.java
@@ -1,28 +1,36 @@
 package org.project.sellbook.controller;
 
-
 import org.project.sellbook.model.User;
 import org.project.sellbook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
-    public String registration(User user) {
+    public String registration(User user, Model model) {
+        String userName = user.getUsername();
+        User isUserNameExist = userRepository.findUserByUsername(userName);
+
+        /* check if there is already username in the database have taken */
+        if (isUserNameExist != null) {
+            model.addAttribute("userNameAlreadyExist", "User already taken. Please choose another username.");
+            return "registration_page";
+        }
+
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole("USER");
-        repository.save(user);
+        userRepository.save(user);
         return "login";
     }
 
